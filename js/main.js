811168d3bd77874c62b4845f0a542d3f3faaa2e8
Jason Lloyd <jason.lloyddev@gmail.com>
2016-12-11 22:21:52 +0000

    Color changes for rows when reaching goals
    If the user reaches their goal the row turns green,
    If they do it every day that week it turns gold!, tested with reaching
    goal and not

--- js/main.js
+++ js/main.js
@@ -3,35 +3,46 @@ var app1 = angular.module('app1',[]);
 // Example of dependency injection, angularjs sees that we need scope so it injects it
 app1.controller('gameCtrl', function($scope, $filter){
 
-    $scope.daysOfWeek = ['Mon','Tues','Wed','Thurs','Fri'];
+    $scope.daysOfWeek = ['Mon','Tues','Wed','Thurs','Fri','Sat','Sun'];
     $scope.listOfHabits = [
       {
         habit:'Wakup at sunrise',
         isDone: [false,false,false,false,false],
-        max:15,
+        goal:15,
         current: 0,
-        difficulty: 5
+        difficulty: 5,
+        color: ''
       },
       {
         habit:'Work on side project',
         isDone: [false,false,false,false,false],
-        max:12,
+        goal:12,
         current: 0,
-        difficulty: 3
+        difficulty: 3,
+        color: ''
       },
       {
         habit:'Go into the office',
         isDone: [false,false,false,false,false],
-        max:15,
+        goal:15,
         current: 0,
-        difficulty: 5
+        difficulty: 5,
+        color: ''
       }
     ];
 
     $scope.changeState = function(rowIndex){
-      console.log(rowIndex);
       var done = $filter("filter")($scope.listOfHabits[rowIndex].isDone, true);
       $scope.listOfHabits[rowIndex].current = done.length *  $scope.listOfHabits[rowIndex].difficulty;
+      if ($scope.listOfHabits[rowIndex].current >= $scope.listOfHabits[rowIndex].goal){
+        if ($scope.listOfHabits[rowIndex].current == $scope.listOfHabits[rowIndex].difficulty*7)
+          $scope.listOfHabits[rowIndex].color = 'gold';
+        else
+          $scope.listOfHabits[rowIndex].color = 'green';
+      }
+      else{
+        $scope.listOfHabits[rowIndex].color = '';
+      }
     }
 
     $scope.weeksPlayingGame = [1];
