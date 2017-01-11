eb6cade76456191c156632cd9d322a66c7a6b02d
Jason Lloyd <jason.lloyddev@gmail.com>
2017-01-11 23:40:56 +0000

    Day 10/365: Refactor Refactor Refactor + All tracked week habits are shown straight away
    - Clean up a bunch of duplicate code
    - When the user reloads the page it knows the last week they tracked and
    all their weeks up until then is shown staight away

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,6 @@
 var app1 = angular.module('app1',['ngMessages','ngCookies']);
 
-
-displayStartMenu = true;
 hasCompletedStartMenu = false;
-
 habitsList = [{
   habit: 'Default',
   difficulty: 0,
@@ -24,28 +21,18 @@ app1.controller('setup',function($scope,$rootScope,$http,$cookies){
   $scope.diffModels = [];
   $scope.daysModels = [];
   $scope.userHasNotVisited = true;
-  $scope.createHabits = function(){
-    habitsList = [];
-    habitsList.push(addHabit($scope.habitModels[0],$scope.diffModels[0],$scope.daysModels[0]))
-    habitsList.push(addHabit($scope.habitModels[1],$scope.diffModels[1],$scope.daysModels[1]))
-    habitsList.push(addHabit($scope.habitModels[2],$scope.diffModels[2],$scope.daysModels[2]))
 
-    $cookies.putObject('defaultHabits',habitsList);
+  if($cookies.getObject('defaultHabits') != undefined){
     $scope.userHasNotVisited = false;
     hasCompletedStartMenu = true;
-
-    $http({
-      method: 'POST',
-      url: '/saveHabits',
-      data: angular.toJson(habitsList),
-      headers: {'Content-type': 'application/json'}
-    }).then(function successCallback(response){
-    }, function errorCallback(response){;
-
-    });
   }
 
-  if($cookies.getObject(1) != undefined){
+  $scope.createHabits = function(){
+    habitsList = [];
+    for(var i=0;i<3;i++)
+      habitsList.push(addHabit($scope.habitModels[i],$scope.diffModels[i],$scope.daysModels[i]))
+
+    $cookies.putObject('defaultHabits',habitsList);
     $scope.userHasNotVisited = false;
     hasCompletedStartMenu = true;
   }
@@ -59,96 +46,74 @@ app1.controller('setup',function($scope,$rootScope,$http,$cookies){
   }
 });
 
-app1.controller('parent',function($scope){
-
+app1.controller('parent',function($scope,$cookies){
   var initialIncrement = 0;
   $scope.weeksPlayingGame = [];
-  $scope.currentSelected = 0;
   currentNum = 1;
-  var maxNumberOfWeeks = 36;
-  // Fill array with values from 0 -> maxNumberOfWeeks
-  $scope.weekOptions = Array.apply(null, {length: maxNumberOfWeeks}).map(Number.call, Number)//new Array(maxNumberOfWeeks);
 
   $scope.incrementWeek = function(){
     $('#intro').hide();
+    $cookies.putObject('totalWeeksTracked',currentNum);
     $scope.weeksPlayingGame.push(currentNum++);
+  }
+
+  if($cookies.getObject('totalWeeksTracked') != undefined){
+    var totalWeeksTracked = parseInt($cookies.getObject('totalWeeksTracked'));
+    for(var i=0;i<totalWeeksTracked;i++){
+      $scope.incrementWeek();
+    }
   }
 
   $scope.decrementWeek = function(){
     $scope.weeksPlayingGame.pop();
     currentNum--;
-
   }
 
-  // each time the dropdown is updated this runs
-  $scope.updateTotal = function(){
-    // TO DO : Has to be a better way to do this than filling an array with the current selected number
-    amountOfTablesToDisplay = Array(parseInt($scope.currentSelected));
-    amountOfTablesToDisplay.fill(1);
-    $scope.weeksPlayingGame = amountOfTablesToDisplay;
-  }
-
-  $scope.display = function(){
+  $scope.hasEnteredHabits = function(){
     return hasCompletedStartMenu;
   }
 });
-// Example of dependency injection, angularjs sees that we need scope so it injects it
-app1.controller('gameCtrl', function($scope, $filter,$cookies){
+
+var counter = 1;
+app1.controller('gameCtrl', function($scope, $filter, $cookies){
     $scope.daysOfWeek = ['Mon','Tues','Wed','Thurs','Fri','Sat','Sun'];
 
     currentController = $scope.weeksPlayingGame.length;
-    if ($cookies.getObject(currentController) != undefined){
-      $scope.listOfHabits = [$cookies.getObject(currentController)[0],
-      $cookies.getObject(currentController)[1],$cookies.getObject(currentController)[2]];
+
+    if ($cookies.getObject(counter) != undefined){
+      $scope.listOfHabits = [$cookies.getObject(counter)[0],
+      $cookies.getObject(counter)[1],$cookies.getObject(counter)[2]];
     }
     else{
       if($cookies.getObject('defaultHabits')){
-        habitsList[0].habit = $cookies.getObject('defaultHabits')[0].habit;
-        habitsList[0].goal = $cookies.getObject('defaultHabits')[0].goal;
-        habitsList[0].difficulty = $cookies.getObject('defaultHabits')[0].difficulty;
-        habitsList[1].habit = $cookies.getObject('defaultHabits')[1].habit;
-        habitsList[1].goal = $cookies.getObject('defaultHabits')[1].goal;
-        habitsList[1].difficulty = $cookies.getObject('defaultHabits')[1].difficulty;
-        habitsList[2].habit = $cookies.getObject('defaultHabits')[2].habit;
-        habitsList[2].goal = $cookies.getObject('defaultHabits')[2].goal;
-        habitsList[2].difficulty = $cookies.getObject('defaultHabits')[2].difficulty;
-      }
-      // need to change the default habits so that when they add a new week that is not in cookies default does show up
-      $scope.listOfHabits = [
-        {
-          habit: habitsList[0].habit,
-          isDone: [false,false,false,false,false],
-          goal: habitsList[0].goal,
-          current: 0,
-          difficulty: habitsList[0].difficulty,
-          color: ''
-        },
-        {
-          habit: habitsList[1].habit,
-          isDone: [false,false,false,false,false],
-          goal: habitsList[1].goal,
-          current: 0,
-          difficulty: habitsList[1].difficulty,
-          color: ''
-        },
-        {
-          habit: habitsList[2].habit,
-          isDone: [false,false,false,false,false],
-          goal: habitsList[2].goal,
-          current: 0,
-          difficulty: habitsList[2].difficulty,
-          color: ''
+        for(var i = 0;i<3;i++){
+          habitsList[i].habit = $cookies.getObject('defaultHabits')[i].habit;
+          habitsList[i].goal = $cookies.getObject('defaultHabits')[i].goal;
+          habitsList[i].difficulty = $cookies.getObject('defaultHabits')[i].difficulty;
         }
-      ];
-  }
+      }
+
+      $scope.listOfHabits = []
+      for(var i = 0;i<3;i++){
+        $scope.listOfHabits.push({
+          habit: habitsList[i].habit,
+          isDone: [false,false,false,false,false],
+          goal: habitsList[i].goal,
+          current: 0,
+          difficulty: habitsList[i].difficulty,
+          color: ''
+        });
+      }
+    }
+    
     $scope.changeState = function(rowIndex,week){
       var completedDays = $filter("filter")($scope.listOfHabits[rowIndex].isDone, true).length;
       var habit = $scope.listOfHabits[rowIndex];
 
       var currentScore = completedDays * habit.difficulty;
       habit.current = currentScore;
-
       var maxScore = habit.difficulty*7;
+
       if (currentScore >= habit.goal){
         if (currentScore == maxScore)
           habit.color = 'gold';
@@ -159,8 +124,7 @@ app1.controller('gameCtrl', function($scope, $filter,$cookies){
         habit.color = '';
       }
 
-    console.log(week);
     $cookies.putObject(week,$scope.listOfHabits);
-    console.log($cookies.getObject(week));
     }
+    counter++;
 });
